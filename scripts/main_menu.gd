57ae78c067c896ac9de23b11ea25d0b6f4c33f11
Nodragem <geoffrey.megardon@outlook.com>
2021-08-22 20:12:50 +0100

    add button

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,13 +1,14 @@
 extends Control
 
 export (Resource) var game_options
+var main_scene
 
 func _ready():
-	pass
+	main_scene = preload("res://scenes/main.tscn")
 
 
 func _on_TextureButton_button_down():
-	get_tree().change_scene("res://scenes/main.tscn")
+	get_tree().change_scene_to(main_scene)
 
 
 func _on_OptionButton_item_selected(index):
